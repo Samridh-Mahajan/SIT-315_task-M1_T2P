a89e295c55fae5f2739457419499904e4c209714
Samridh-Mahajan <133039483+Samridh-Mahajan@users.noreply.github.com>
2024-02-03 15:42:03 +0530

    samridh_mahajan1.ino

--- samridh_mahajan1.ino
+++ samridh_mahajan1.ino
@@ -1,32 +1,42 @@
-// C++ code
-//
-const int pir = 2;
-const int ledPin = 11;
+// Define pin numbers
+const int pir = 2;       // PIR sensor pin
+const int ledPin = 11;   // LED pin
+
+// Flag to indicate motion detection
 volatile bool motionDetected = false;
 
 void setup()
 {
+  // Initialize pin modes
   pinMode(pir, INPUT);
   pinMode(ledPin, OUTPUT);
+  
+  // Initialize serial communication
   Serial.begin(9600);
 
+  // Attach interrupt to PIR sensor pin for motion detection
   attachInterrupt(digitalPinToInterrupt(pir), motionDetectedISR, CHANGE);
 }
 
 void loop()
 {
+  // Empty loop as we're using interrupts for motion detection
 }
 
+// Interrupt Service Routine (ISR) for motion detection
 void motionDetectedISR()
 {
+  // Check if PIR sensor is triggered
   if (digitalRead(pir) == HIGH)
   {
+    // Motion detected
     Serial.println("Motion Detected!");
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH); // Turn on LED
   }
   else
   {
+    // Motion ended
     Serial.println("Motion Ended!");
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW);  // Turn off LED
   }
 }
